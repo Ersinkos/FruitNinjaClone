84901e3d55d10e186439b1db632288ff33dec3b6
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-09-04 11:47:20 +0300

    UI and ParticleEffect added
    Display Score on top left

--- FruitNinjaClone/Assets/Scripts/Target.cs
+++ FruitNinjaClone/Assets/Scripts/Target.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     Rigidbody targetRb;
+    GameManager gameManager;
 
     private float minSpeed = 12.0f;
     private float maxSpeed = 16.0f;
     private float maxTorque = 10.0f;
     private float xRange = 4.0f;
     private float ySpawnPos = -6.0f;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         targetRb = GetComponent<Rigidbody>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
@@ -31,6 +36,9 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+
     }
     private void OnTriggerEnter(Collider other)
     {
